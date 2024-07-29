da40533ead89468b37cefd567f0bb658dcac0a0a
Delano <150105258+delano-gon@users.noreply.github.com>
2024-07-29 13:42:09 -0500

    Added mealdetails fetching function inside lib/meals.js file. Also added jsx and functions for fetching/rendering mealdetails inside [mealSlug]/page.js file.

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,5 +1,34 @@
-export default function MealsSlugPage() {
-  return <main>
-    <h1>Meal Slug</h1>
-  </main>
+import Image from 'next/image';
+import { getMeal } from '@/lib/meals';
+import classes from './page.module.css';
+
+export default function MealsSlugPage({params}) {
+  const meal = getMeal(params.mealSlug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
