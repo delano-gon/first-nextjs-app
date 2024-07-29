d58d46efb593032959e06b499490fe9ff72e69f0
Delano <150105258+delano-gon@users.noreply.github.com>
2024-07-29 13:15:33 -0500

    Added error.js and not-found.js files.

---
+++ app/not-found.js
@@ -0,0 +1,6 @@
+export default function NotFound() {
+  return <main className="not-found">
+    <h1>Page not found!</h1>
+    <p>Sorry, we couldn&apos;t find the page you were looking for.</p>
+  </main>
+}
