a874ce504666d917db128a2985224dfe9ff453e8
Delano <150105258+delano-gon@users.noreply.github.com>
2024-07-29 13:49:43 -0500

    Added code to call notFound function inside [mealSlug]/page.js file for meals that don't exist. Created not-found.js file inside meals folder for more accurate "error" handling.

---
+++ app/meals/not-found.js
@@ -0,0 +1,6 @@
+export default function NotFound() {
+  return <main className="not-found">
+    <h1>Meal not found!</h1>
+    <p>Sorry, we couldn&apos;t find the meal details you were looking for.</p>
+  </main>
+}
